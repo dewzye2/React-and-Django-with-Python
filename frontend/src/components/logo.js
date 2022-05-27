21987fec677ba414cd23d5f741e53035706c28fc
dewzye2 <prankbooh1@gmail.com>
2022-05-27 13:23:14 -0300

    changes

--- frontend/src/components/logo.js
+++
@@ -1,9 +0,0 @@
-// import logo from './logo.png'
-
-// function LogoSP() {
-//   return (
-//   <img src={logo} alt='Not Found!' style={{ maxWidth: '8%'}}/>
-//   )
-// }
-
-// export default LogoSP
